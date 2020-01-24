e7a24ec62b616cc08bc24e940be7d89fb898cbc7
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    vendor: Use and support Pixel charger animation
    Co-authored-by: PainKiller3 <ninadpatil100@gmail.com>
    Co-authored-by: Tim Zimmermann <tim@linux4.de>
    Signed-off-by: Arseniy Graur <argraur@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -146,6 +146,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images \
+    product_charger_res_images_vendor
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig \
