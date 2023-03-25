c89bcea8c89aea5e781e516bf373ebde5a520e06
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-25 15:58:25 +0800

    audio: Import sound effects pack
    * taken from protonAOSP and rice A12
    
    Co-authored-by: kdrag0n <danny@kdrag0n.dev>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,7 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/crdroid.mk)
 $(call inherit-product-if-exists, vendor/addons/config.mk)
 $(call inherit-product-if-exists, external/faceunlock/config.mk)
+$(call inherit-product-if-exists, vendor/lineage/audio/audio.mk)
 
 PRODUCT_BRAND ?= crDroidAndroid
 
