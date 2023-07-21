4cd0d189f82a63ec194cef2e3c309e6ad07f113d
mukesh22584 <mks22584@gmail.com>
2023-07-21 13:22:11 +0530

    vendor: Refactor Matrixx
    - Don't build updater
    - Remove bootanimation product package
    - Add more Matrixx properties
    - Add Official tag support
    - Add build variant
    - Rearrange final zip output name

--- config/common.mk
+++ config/common.mk
@@ -109,8 +109,6 @@ endif
 # Bootanimation
 TARGET_SCREEN_WIDTH ?= 1080
 TARGET_SCREEN_HEIGHT ?= 1920
-PRODUCT_PACKAGES += \
-    bootanimation.zip
 
 # Build Manifest
 PRODUCT_PACKAGES += \
@@ -120,9 +118,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageParts \
     LineageSettingsProvider \
-    LineageSetupWizard \
-    Updater
-
+    LineageSetupWizard
+    
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
