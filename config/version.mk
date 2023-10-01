17483b0611a34d9a398fbc74853af64ac4d0328d
TechMatrixx <techmatrixx34@gmail.com>
2023-10-02 01:42:24 +0530

    vendor: Add Official tag support

--- config/version.mk
+++ config/version.mk
@@ -4,6 +4,18 @@ PRODUCT_VERSION_MINOR = 0
 # Increase MATRIXX Version with each major release.
 MATRIXX_VERSION := 6.0
 
+MATRIXX_BUILD_TYPE ?= Unofficial
+
+ifeq ($(MATRIXX_BUILD_TYPE), Official)
+  OFFICIAL_DEVICES = $(shell cat vendor/lineage/matrixx.devices)
+  FOUND_DEVICE =  $(filter $(LINEAGE_BUILD), $(OFFICIAL_DEVICES))
+    ifeq ($(FOUND_DEVICE),$(LINEAGE_BUILD))
+      MATRIXX_BUILD_TYPE := Official
+    else
+      MATRIXX_BUILD_TYPE := Unofficial
+    endif
+endif
+
 # Internal version
 LINEAGE_VERSION := Matrixx-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(MATRIXX_VERSION)
 
