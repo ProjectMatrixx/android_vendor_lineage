0b84bd77b2bcc3c7d0f95afcb7bb141477c97d0c
Dyneteve <dyneteve@hentaios.com>
2023-11-17 21:08:42 +0530

    vendor: Build the textclassifier models
    Change-Id: I9d7d582ddc427998e68e668cc0375958325d8052
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -285,6 +285,13 @@ PRODUCT_PACKAGES += \
     NetworkStackOverlay \
     PermissionControllerOverlay
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_annotator_en_model \
+    libtextclassifier_annotator_universal_model \
+    libtextclassifier_actions_suggestions_universal_model \
+    libtextclassifier_lang_id_model
+
 # Translations
 CUSTOM_LOCALES += \
     ast_ES \
