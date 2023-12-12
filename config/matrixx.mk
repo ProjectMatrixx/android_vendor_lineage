efb11217ed371f66a9a40cb6cfe07bc309846f64
mukesh22584 <mks22584@gmail.com>
2023-12-12 17:17:21 +0530

    config: Don't build MatLog
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- config/matrixx.mk
+++ config/matrixx.mk
@@ -70,7 +70,6 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 PRODUCT_PACKAGES += \
     BatteryStatsViewer \
     GameSpace \
-    MatLog \
     OmniJaws \
     OmniStyle
 
