3e8cc20eee30737b3d074c2249ae1947cad4a2fd
mukesh22584 <mks22584@gmail.com>
2023-12-12 17:17:21 +0530

    config: Don't build MatLog

--- config/matrixx.mk
+++ config/matrixx.mk
@@ -70,7 +70,6 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 PRODUCT_PACKAGES += \
     BatteryStatsViewer \
     GameSpace \
-    MatLog \
     OmniJaws
 
 ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
