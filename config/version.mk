79cb47eaec084a295de2c873ec1ad4228fce689c
mukesh22584 <mks22584@gmail.com>
2023-12-12 17:10:26 +0530

    vendor: Initial Matrixx conversion

--- config/version.mk
+++ config/version.mk
@@ -1,11 +1,33 @@
 PRODUCT_VERSION_MAJOR = 14
 PRODUCT_VERSION_MINOR = 0
 
-# Increase CR Version with each major release.
-CR_VERSION := 10.5
+# Increase MATRIXX Version with each major release.
+MATRIXX_VERSION := 10.5.1
+
+MATRIXX_BUILD_TYPE ?= Unofficial
+
+ifeq ($(WITH_GMS), true)
+  MATRIXX_BUILD_VARIANT := Gapps
+else
+  MATRIXX_BUILD_VARIANT := Vanilla
+endif
+
+ifeq ($(MATRIXX_BUILD_TYPE), Official)
+-include vendor/lineage-priv/keys/keys.mk
+  OFFICIAL_DEVICES = $(shell cat vendor/lineage/matrixx.devices)
+  FOUND_DEVICE =  $(filter $(LINEAGE_BUILD), $(OFFICIAL_DEVICES))
+    ifeq ($(FOUND_DEVICE),$(LINEAGE_BUILD))
+      MATRIXX_BUILD_TYPE := Official
+    else
+      MATRIXX_BUILD_TYPE := Unofficial
+    endif
+endif
 
 # Internal version
-LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)
+LINEAGE_VERSION := Matrixx$(MATRIXX_VARIANT)-v$(MATRIXX_VERSION)-$(MATRIXX_BUILD_TYPE)-$(LINEAGE_BUILD)-$(MATRIXX_BUILD_VARIANT)-$(shell date +%Y%m%d)
 
 # Display version
-LINEAGE_DISPLAY_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-v$(CR_VERSION)
+LINEAGE_DISPLAY_VERSION := Matrixx-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-v$(MATRIXX_VERSION)
+
+# Build info
+MATRIXX_BUILD_INFO := $(LINEAGE_VERSION)
