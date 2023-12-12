6a6a077dc56d77b961d9d21d8ad1dc6753871f10
mukesh22584 <mks22584@gmail.com>
2023-12-12 17:10:26 +0530

    vendor: Initial Matrixx conversion

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -4,13 +4,18 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
-# crDroid System Version
+# Matrixx properties
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.crdroid.build.version=$(LINEAGE_VERSION) \
-    ro.crdroid.display.version=$(LINEAGE_DISPLAY_VERSION) \
-    ro.crdroid.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(CR_VERSION) \
-    ro.crdroidlegal.url=https://crdroid.net/legal.php
+    ro.matrixx.battery?=$(MATRIXX_BATTERY) \
+    ro.matrixx.build.variant=$(MATRIXX_BUILD_VARIANT) \
+    ro.matrixx.build.version=$(LINEAGE_VERSION) \
+    ro.matrixx.chipset?=$(MATRIXX_CHIPSET) \
+    ro.matrixx.display_resolution?=$(MATRIXX_DISPLAY) \
+    ro.matrixx.display.version=$(LINEAGE_DISPLAY_VERSION) \
+    ro.matrixx.maintainer=$(MATRIXX_MAINTAINER) \
+    ro.matrixx.release.type=$(MATRIXX_BUILD_TYPE) \
+    ro.matrixx.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(MATRIXX_VERSION)
 
 # LineageOS Platform SDK Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
