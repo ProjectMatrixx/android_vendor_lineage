62530aafa6b3b0a15093ee41ea54ead2196a6f7c
John Galt <johngaltfirstrun@gmail.com>
2024-01-02 11:36:34 -0500

    matrixx: Add preinstalled package list to stop auto cloning
    Only extra adds needed, gms now ships it's own lists for this.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/matrixx.mk
+++ config/matrixx.mk
@@ -45,6 +45,10 @@ ifeq ($(WITH_GMS),true)
 $(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
 endif
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml
+
 # Disable async MTE on system_server
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     arm64.memtag.process.system_server=off
