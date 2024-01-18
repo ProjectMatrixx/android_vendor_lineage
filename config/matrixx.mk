350e97056f01b840338e617d2d064004bd410938
mukesh22584 <mks22584@gmail.com>
2024-01-18 10:55:29 +0530

    overlay: Add MatrixxOTA support for vanila builds

--- config/matrixx.mk
+++ config/matrixx.mk
@@ -45,6 +45,11 @@ ifeq ($(WITH_GMS),true)
 $(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
 endif
 
+ifneq ($(WITH_GMS),true)
+PRODUCT_PACKAGES += \
+    MatrixxOtaVanila
+endif
+
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml
