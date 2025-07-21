a7dcffa0c5be7b7ee52dfa6273dc42749c7c59f0
Aaron Kling <webgeek1234@gmail.com>
2025-07-21 00:32:21 -0500

    common: Allow vendors to exclude bp paths
    This resolves a couple issues. Allowing bp's that don't resolve to be
    in-tree. And allowing multiple copies of the same target.
    
    Change-Id: I85cfc566e18ea9fe1cb4cd8d70a71ef80517fd41
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,9 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/matrixx.mk)
 $(call inherit-product-if-exists, vendor/addons/config.mk)
 
+# Allow vendor prebuilt repos to exclude themselves from bp scanning
+-include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
+
 PRODUCT_BRAND ?= ProjectMatrixx
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
