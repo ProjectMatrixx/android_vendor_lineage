c0d7a2f4a9ba3804e71cf7cab00d3f9e0a682b1c
drkphnx <dark.phnx12@gmail.com>
2025-11-02 13:18:29 +0000

    drop crdroid offline charging animation * inorder to bring pixel charging manimation

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -48,20 +48,6 @@ ifneq ($(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS),)
     $(error TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS is deprecated, please migrate to soong_config_set,camera,needs_client_info_lib_oplus)
 endif
 
-# Charger
-lineage_charger_density := mdpi
-ifneq (,$(TARGET_SCREEN_DENSITY))
-lineage_charger_density := $(strip \
-  $(or $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 560))),1),xxxhdpi),\
-       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 400))),1),xxhdpi),\
-       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 280))),1),xhdpi),\
-       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 200))),1),hdpi,mdpi)))
-else ifneq (,$(filter mdpi hdpi xhdpi xxhdpi xxxhdpi,$(PRODUCT_AAPT_PREF_CONFIG)))
-# If PRODUCT_AAPT_PREF_CONFIG includes a dpi bucket, then use that value.
-lineage_charger_density := $(PRODUCT_AAPT_PREF_CONFIG)
-endif
-$(call soong_config_set,lineage_charger,density,$(lineage_charger_density))
-
 # Libui
 ifneq ($(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS),)
     $(call soong_config_set,libui,additional_gralloc_10_usage_bits,$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -37,16 +37,6 @@ endif
 PRODUCT_PACKAGES += \
     Launcher3Overlay
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-ifneq ($(WITH_LINEAGE_CHARGER),false)
-PRODUCT_PACKAGES += \
-    lineage_charger_animation \
-    lineage_charger_animation_vendor
-endif
-
 # Legal
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.crdroidlegal.url=https://crdroid.net/legal.php
